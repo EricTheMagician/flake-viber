347c6dc8827fdd7b74cd1b67b63105c70c857814
Eric Yen <eric@ericyen.com>
2023-08-16 03:01:41 +0000

    added a formatter

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    };
+  };
+
   outputs = { self, nixpkgs }: {
-
-
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
   };
 }
